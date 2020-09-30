7d008375cda5aa85d4aa1def280a9b07b628ef7f
Nathan Fisk <triphazard@gmail.com>
2020-09-30 15:33:58 +0100

    Webpack working with template

--- jsintegration/settings.py
+++ jsintegration/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -56,7 +57,6 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
-        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
@@ -64,6 +64,12 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'loaders': [
+            (
+                'django.template.loaders.filesystem.Loader',
+                [os.path.join(BASE_DIR, 'templates')],
+            ),
+        ],
         },
     },
 ]
@@ -119,3 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
